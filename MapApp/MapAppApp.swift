73fd720312f1637e5120e34ca0180e3ee0ddcb02
Ali Görkem Aksöz <a.gorkemaksoz@gmail.com>
2023-07-09 16:54:47 +0300

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Ali Görkem Aksöz on 9.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
